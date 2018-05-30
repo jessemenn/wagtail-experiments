66f42ba82f9860646bfd2efe6ddeb2f5635697fa
jymenn@syr.edu <jymenn@syr.edu>
2018-05-30 09:19:07 -0400

    This will work in wagtail 2.0 and django 2.0,maybe not 1.11

--- experiments/admin_urls.py
+++ experiments/admin_urls.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import url
 from experiments import views
-
+app_name = 'experiments'
 urlpatterns = [
     url(r'^experiment/report/(\d+)/$', views.experiment_report, name='report'),
     url(r'^experiment/select_winner/(\d+)/(\d+)/$', views.select_winner, name='select_winner'),
--- experiments/migrations/0001_initial.py
+++ experiments/migrations/0001_initial.py
@@ -29,8 +29,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
                 ('name', models.CharField(max_length=255)),
                 ('slug', models.SlugField(max_length=255)),
-                ('control_page', models.ForeignKey(related_name='+', to='wagtailcore.Page')),
-                ('goal', models.ForeignKey(related_name='+', to='wagtailcore.Page')),
+                ('control_page', models.ForeignKey(related_name='+', to='wagtailcore.Page', on_delete=models.CASCADE)),
+                ('goal', models.ForeignKey(related_name='+', to='wagtailcore.Page', on_delete=models.CASCADE)),
             ],
             options={
                 'abstract': False,
@@ -44,6 +44,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='alternative',
             name='page',
-            field=models.ForeignKey(related_name='+', to='wagtailcore.Page'),
+            field=models.ForeignKey(related_name='+', to='wagtailcore.Page', on_delete=models.CASCADE), 
         ),
     ]
